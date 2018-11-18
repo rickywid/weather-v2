aa85a98289b14f64834eeb0980431a1c752d35a9
ricky jw <rickywid@hotmail.com>
2018-11-17 22:48:50 -0500

    add chartjs

--- src/components/home.js
+++ src/components/home.js
@@ -2,15 +2,14 @@ import React from 'react';
 import QuickCity from './/quick-city';
 import Slider from "react-slick";
 import Button from '@material-ui/core/Button';
+import {Line} from 'react-chartjs-2';
+import moment from 'moment';
 
 export default class Home extends React.Component {
 
 
 	componentDidMount() {
-
-
-		let days = [this.props.location.week[3], this.props.location.week[11], this.props.location.week[19], this.props.location.week[35]];
-
+		console.log(this.props.location.week)
 	}
 
     displayMyCities() {
@@ -33,13 +32,34 @@ export default class Home extends React.Component {
 
 	}
 	render() {
-	    var settings = {
+	    const settings = {
 	      dots: false,
 	      infinite: false,
 	      speed: 500,
 	      slidesToShow: 5,
 	      slidesToScroll: 1
 	    };
+
+		var chartData = {
+			labels: [	moment(this.props.location.week[3].dt_txt).format("ll"), 
+						moment(this.props.location.week[11].dt_txt).format("ll"), 
+						moment(this.props.location.week[19].dt_txt).format("ll"), 
+						moment(this.props.location.week[27].dt_txt).format("ll"), 
+						moment(this.props.location.week[35].dt_txt).format("ll")
+					],
+
+			datasets: [
+				{
+					label: "5 DAY FOREACAST",
+					data: [	Math.ceil(this.props.location.week[3].main.temp - 273.15), 
+							Math.ceil(this.props.location.week[11].main.temp - 273.15), 
+							Math.ceil(this.props.location.week[19].main.temp - 273.15), 
+							Math.ceil(this.props.location.week[27].main.temp - 273.15), 
+							Math.ceil(this.props.location.week[35].main.temp - 273.15)
+					]
+				}
+			]
+		};
 
 		return (
 			<div className="body">
@@ -60,12 +80,20 @@ export default class Home extends React.Component {
 						<p>{this.props.location.weather.desc}</p>
 						{this.displaySaveBtnCity()}
                     </div>
+
+
+
+
+
                     <div className="user-temp__side">
 						<h3>5 day</h3>
+
 						<h3>temp</h3>
+						<Line data={chartData} />
                     </div>
 				</div>
 			</div>
 		)
 	}
 }
+
