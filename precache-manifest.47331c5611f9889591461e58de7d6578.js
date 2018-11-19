41e76c5bef33f8b3754f292f7f5cf76c1590d7dc
ricky jw <rickywid@hotmail.com>
2018-11-19 03:36:44 -0500

    Updates

---
+++ precache-manifest.47331c5611f9889591461e58de7d6578.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/openweather/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/openweather/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/openweather/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/openweather/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "e8f68946e9cd595ec1fa",
+    "url": "/openweather/static/js/runtime~main.e8f68946.js"
+  },
+  {
+    "revision": "45ffc470eb2200c05a18",
+    "url": "/openweather/static/js/main.45ffc470.chunk.js"
+  },
+  {
+    "revision": "159a2278eff3c15c5743",
+    "url": "/openweather/static/js/1.159a2278.chunk.js"
+  },
+  {
+    "revision": "45ffc470eb2200c05a18",
+    "url": "/openweather/static/css/main.2b78d09e.chunk.css"
+  },
+  {
+    "revision": "159a2278eff3c15c5743",
+    "url": "/openweather/static/css/1.3250e48f.chunk.css"
+  },
+  {
+    "revision": "8b591921feccd10c7a01a5bdb1fc5b03",
+    "url": "/openweather/index.html"
+  }
+];
